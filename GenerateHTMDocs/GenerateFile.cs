2cab4c5d1bc39263bebbbe81bace1f543360e3c5
SirJUST <theoperaking@yahoo.com>
2019-04-17 07:20:20 -0400

    modify output file to point to correct image and links

--- GenerateHTMDocs/GenerateFile.cs
+++ GenerateHTMDocs/GenerateFile.cs
@@ -11,11 +11,26 @@ namespace GenerateHTMDocs
     {
         public GenerateFile(Document baseFile)
         {
-            string newFileName = baseFile.fileName + "new";
-            StreamWriter sw = new StreamWriter(baseFile.path.Remove(baseFile.path.Length-5) + "3.htm");
-            for(int i = 0; i< baseFile.lines.Count; i++)
+            Document newPage = new Document();
+            newPage.fileNumber = baseFile.fileNumber + 1;
+            newPage.fileName = "Page" + newPage.fileNumber;
+            newPage.path = baseFile.path.Remove(baseFile.path.Length - 5) + newPage.fileNumber + ".htm";
+            newPage.lines = new List<string>();
+            foreach(string line in baseFile.lines)
             {
-                sw.WriteLine(baseFile.lines[i]);
+                newPage.lines.Add(line);
+            }
+            // change image reference
+            newPage.lines[8] = newPage.lines[8].Substring(0,37) + "3" + newPage.lines[8].Substring(38);
+
+            //change forward and backward links
+            newPage.lines[10] = newPage.lines[10].Remove(newPage.lines[10].Length - 7) + (newPage.fileNumber - 1) + ".htm" + '\u0022' +">";
+            newPage.lines[11] = newPage.lines[11].Remove(newPage.lines[11].Length - 7) + (newPage.fileNumber + 1) + ".htm" + '\u0022' + ">";
+
+            StreamWriter sw = new StreamWriter(newPage.path);
+            for(int i = 0; i< newPage.lines.Count; i++)
+            {
+                sw.WriteLine(newPage.lines[i]);
             }
             sw.Close();
         }
